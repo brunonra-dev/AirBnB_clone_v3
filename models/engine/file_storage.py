335a22bd26da7b7604679894f86dd7e17080bad8
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-21 17:11:10 -0300

    Revert "Test task 3"
    This reverts commit 9973a1405ab3bd110a82a21f66426772c2081c7f.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,19 +68,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def get(self, cls, id):
-        """Returns the object based on the class and its ID
-        Otherwise returns None"""
-        objs = self.all(cls)
-        if objs:
-            return objs['{}.{}'.format(cls.__name__, id)]
-        else:
-            return None
-
-    def count(self, cls=None):
-        """Returns the number of objects in
-        storage matching the given class
-        If no class is passed, returns the
-        count of all objects in storage."""
-        return len(self.all(cls))
