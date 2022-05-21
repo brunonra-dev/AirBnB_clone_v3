4bac82f3c92771c183f7441a89df4442a3f58b32
AngelPiva <angelpiva6@gmail.com>
2022-05-21 22:40:07 +0000

    port getenv int

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,5 +19,5 @@ def not_found(e):
 
 if __name__ == '__main__':
     app.run(host=getenv('HBNB_API_HOST', default='0.0.0.0'),
-            port=getenv('HBNB_API_PORT', default='5000'),
+            port=int(getenv('HBNB_API_PORT', default='5000')),
             threaded=True)
