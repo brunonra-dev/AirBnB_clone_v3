5656ad1b5eddd1c3e62defbc99cf903d8528500d
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-21 19:06:22 -0300

    Fix 404 & env var

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """
 starts a Flask web application
 """
+from os import getenv
 from models import storage
 from api.v1.views import app_views
 from api.v1.views.index import *
@@ -11,6 +12,11 @@ from flask import Flask
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify(error="Not found"), 404
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True, debug=True)
+    app.run(host=getenv('HBNB_API_HOST', default='0.0.0.0'),
+    port=getenv('HBNB_API_PORT', default='5000'), threaded=True, debug=True)
